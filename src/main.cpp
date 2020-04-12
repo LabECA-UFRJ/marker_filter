ed3819288c05f9a992e376d2d563c7a5c3ac3d1b
nori12 <phbnori@gmail.com>
2020-04-11 22:46:21 -0300

    tf2 migration done in order to correct rotations due to camera fixed frame

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,36 @@
 #include "ros/ros.h"
 #include "aruco_msgs/MarkerArray.h"
-#include "geometry_msgs/PoseStamped.h"
-
-#include <iostream>
-#include <inttypes.h>
-#include <cmath>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
 
 using namespace std;
 
-ros::Publisher pub;
 int id;
+string frame_name;
 
 void markerReceived(const aruco_msgs::MarkerArray::ConstPtr &markers)
 {
     for (int i = 0; i < markers->markers.size(); i++) {
         if (markers->markers[i].id == id) {
-            geometry_msgs::PoseStamped pose;
-            pose.pose.position = markers->markers[i].pose.pose.position;
-            pose.pose.orientation = markers->markers[i].pose.pose.orientation;
+            static tf2_ros::TransformBroadcaster br;
+            geometry_msgs::TransformStamped transformStamped;
 
-            pub.publish(pose);
+            transformStamped.header.stamp = ros::Time::now();
+            // In our case, the world frame is the camera default frame
+            transformStamped.header.frame_id = "world";
+            transformStamped.child_frame_id = frame_name;
+
+            // This swaps and minus signals makes no sense but works
+            transformStamped.transform.translation.x = -markers->markers[i].pose.pose.position.x;
+            transformStamped.transform.translation.y = -markers->markers[i].pose.pose.position.z;
+            transformStamped.transform.translation.z = -markers->markers[i].pose.pose.position.y;
+
+            transformStamped.transform.rotation.x = markers->markers[i].pose.pose.orientation.x;
+            transformStamped.transform.rotation.y = markers->markers[i].pose.pose.orientation.y;
+            transformStamped.transform.rotation.z = -markers->markers[i].pose.pose.orientation.z;
+            transformStamped.transform.rotation.w = markers->markers[i].pose.pose.orientation.w;
+
+            br.sendTransform(transformStamped);
         }
     }
 }
@@ -35,7 +46,11 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    pub = nodeHandle.advertise<geometry_msgs::PoseStamped>("pose", 5);
+    if (ros::param::get("~frame_name", frame_name) == false)
+    {
+        ROS_FATAL("Parameter frame_name not set.");
+        return -1;
+    }    
 
     ros::Subscriber sub = nodeHandle.subscribe("markers", 1, markerReceived);
 
